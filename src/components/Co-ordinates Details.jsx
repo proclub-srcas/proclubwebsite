053f7e4a633dd482f92725c706d29ee21e74ec19
Tino Britty <153193545+brittytino@users.noreply.github.com>
2024-07-01 19:09:32 +0530

    Update Co-ordinates Details.jsx

--- src/components/Co-ordinates Details.jsx
+++ src/components/Co-ordinates Details.jsx
@@ -25,7 +25,7 @@ export const staffCoordinators = [
 ];
 
 export const officeBearers = [
-    { role: 'Chairman', name: 'Tino Britty J', img: chairman, dept: 'B.Sc CS',  instagram:'https://www.instagram.com/brittytino/' , linkedin:'https://www.linkedin.com/in/brittytino', portfolio:'https://tinobritty.web.app/'},
+    { role: 'Chairman', name: 'Tino Britty J', img: chairman, dept: 'B.Sc CS',  instagram:'https://www.instagram.com/brittytino/' , linkedin:'https://www.linkedin.com/in/brittytino', portfolio:'https://tinobritty.tech/'},
     { role: 'Vice Chairman', name: 'John Samuel', img: assistantChairman, dept: 'B.Sc IT', instagram:'https://www.instagram.com/samjohn_offl' , linkedin:'https://www.linkedin.com/in/samjohnoffl', portfolio:'https://portfolio.johnsamuel.in/' },
     { role: 'Secretary', name: 'Pravin B', img: secretary, dept:'B.Sc CS & DA', instagram : 'https://www.instagram.com/bpravin._/' , linkedin : 'https://www.linkedin.com/in/pravinboopathi/' , portfolio : 'https://pravin-boopathi.web.app/' },
     { role: 'Joint Secretary', name: 'Kanishka C', img: viceChairman, dept:'B.Sc CS (AI & DS)', instagram:'https://www.instagram.com/kaniskha_chandrasekar/' , linkedin: 'https://www.linkedin.com/in/kaniskha-c-2124a5292/'},
